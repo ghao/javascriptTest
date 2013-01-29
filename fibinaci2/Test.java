4b958ac5e67a43b10cdce38d4b8cc1ced1b2053e
colorful <332234055@qq.com>
2013-01-29 12:20:43 +0800

    int and bool?

---
+++ fibinaci2/Test.java
@@ -0,0 +1,13 @@
+public class Test{
+	public static void main (String args[]){
+	int i=40;
+	int j=2*100;
+	if (i>1){
+	System.out.println(i);
+	System.out.println(j+i);
+} else{
+		i=30+i;
+	System.out.println(i);
+}
+}
+}
