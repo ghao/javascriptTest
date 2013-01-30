51d6e1bb7f33691cac8c7f3c9d2e3994253e810a
colorful <332234055@qq.com>
2013-01-30 20:44:34 +0800

    life as a ..

---
+++ myroad/life.c
@@ -0,0 +1,42 @@
+把生活想象成一个在空中抛接五只球的游戏。
+Imagine life as a game in which you are juggling some five balls in the air.
+这五只球被你分别称为：工作、家庭、健康、友谊和精神。你努力不让它们从空中掉下来。
+You name them: Work, Family, Health, Friends, Spirit. And you re keeping all of these in the air.
+你马上就发现惟有工作是一个橡皮球。
+You will soon understand that work is a rubber ball.
+如果你将它掉到地上，它还会弹回来。但其它四只球——家庭、健康、朋友和精神都是玻璃的。
+If you drop it, it will bounce back. But the other four balls-family, health, friends and spirit are made of glass.
+如果你把任何一个掉到地上，便会不可挽回地留下疤痕、裂缝、受损甚至摔得粉碎。
+If you drop one of these, they will be irrevocably scuffed , marked, nicked , damaged or even shattered .
+它们永远也无法恢复原样。你必须懂得这一点并在生活中努力保持平衡。
+They will never be the same. You must understand that and strive for balance in your life.
+怎样做到这一点呢？
+How?
+不要总拿自己与别人比从而贬低自身的价值。正因为我们彼此的差异不同，我们才会各有千秋。
+Don't undermine your worth by comparing yourself with others. It is because we are different that each of us is special. 
+别人认为重要，并不能作为你确定目标的根据。只有你自己知道，什么东西对你最好。
+Don't set your goals by what other people deem important. Only you know what is best for you. 
+与你内心最贴近的东西，切莫等闲视之。要像坚守生命一样坚守住它们，因为失去它们，生活就会毫无意义。
+Don t take for granted the things closest to your heart. Cling to them as you would cling to your life, for without them, life is meaningless.
+不要耽于过去或未来，而让生命从指间悄悄溜走。一次只活一天，活在今日，你就充实地活过生命中的每一天。
+Don t let your life slip through your fingers by living in the past or for the future.By living your life one day at a time, you live all the days of your life. 
+如果你还可以付出，就不要轻言放弃。在你停止努力那一刻之前，什么都没有真正结束。
+Don't give up when you still have something to give. Nothing is really over until the moment you stop trying. 
+正因为我们都不完美，我们才会彼此需要。
+Don't be afraid to admit that you are less than perfect. It is this fragile thread that binds us each together. 
+不要害怕遭遇风险。只有通过冒险，我们才能学会如何变得勇敢。
+Don't be afraid to encounter risks. It is by taking chances that we learn how to be brave.
+别说真爱难求，而将爱拒之门于生活之外。获得爱的捷径是给予爱，失去爱的捷径是扼住爱，而守住爱的捷径是给爱插上翅膀。
+Don't shut love out of your life by saying it s impossible to find. The quickest way to receive love is to give; the fastest way to lose love is to hold it too tightly; and the best way to keep love is to give it wings.
+不要一生奔忙，匆匆而过，以至于忘了自己从何而来，往何而去。
+Don't run through life so fast that you forget not only where you ve been, but also where you are going.
+不要忘记一个人最大的感情需要是得到别人的理解。
+Don't forget, a person's greatest emotional need is to feel appreciated.
+不要害怕学习。知识没有重量：它是你随时可以轻松携带的宝藏。
+Don't be afraid to learn. Knowledge is weightless, a treasure you can always carry easily.
+不要漫不经心地打发时间或口无遮拦地说话，失去的时间和说出去的话都无法挽回。
+Don't use time or words carelessly. Neither can be retrieved.
+生活不是一场赛跑，而是每一步都宜细细品尝的人生旅程。
+Life is not a race, but a journey to be savored each step of the way.
+昨天属于历史。明天是未知的谜。而今天是赐予我们的礼物：所以我们把“今天”称之为"礼物"。
+Yesterday is history, Tomorrow is a mystery and Today is a gift: that's why we call it "The Present"
